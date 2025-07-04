e28f48478a1a1b45ce2f83c978d5bc2c87f77775
Mister <69367697+Dublin7@users.noreply.github.com>
2025-07-04 05:42:34 +0100

    Create MainActivity.kt

---
+++ app/app/src/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mobiledevapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val textView = TextView(this)
+        textView.text = "Hello from Mobile Dev App!"
+        setContentView(textView)
+    }
+}
